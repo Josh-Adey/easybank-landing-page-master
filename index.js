115231a17bcc13705ed15a88edf1dc50194aa91a
Joshua <robertjoshua905@gmail.com>
2022-09-12 19:46:35 -0700

    Footer fixed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 let toggleDisplay = document.querySelector(".mobile-nav-items");
 let clicked = false;
+
 
 function toggleSwitch() {
  
@@ -8,25 +8,12 @@ function toggleSwitch() {
 
         toggleDisplay.style.display = "block";
         clicked = false;
+
     }
     else {
         toggleDisplay.style.display = "none";
         clicked = true;
+        
+        
     }
-=======
-let toggleDisplay = document.querySelector(".mobile-nav-items");
-let clicked = false;
-
-function toggleSwitch() {
- 
-    if (clicked) {
-
-        toggleDisplay.style.display = "block";
-        clicked = false;
-    }
-    else {
-        toggleDisplay.style.display = "none";
-        clicked = true;
-    }
->>>>>>> c49e89d52cde6d3589edae6c5f654afad275ecbd
 }
